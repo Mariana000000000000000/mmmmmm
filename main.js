45ff947de77b362aea721cd6afda9ddb2c7442ba
Mari <142262838+Mariana000000000000000@users.noreply.github.com>
2024-10-16 12:20:06 -0300

    gt ht

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container');
     let cartao = document.createElement('article')
-    cartao.className= 'cartao';
+    cartao.className = 'cartao';
     cartao.innerHTML = `<div class="conteudo-cartao">
     <h3>${categoria}</h3>
     <div class="pergunta-cartao">
@@ -11,6 +11,13 @@ function criaCartao(categoria, pergunta, resposta) {
         <p>${resposta}</p>
     </div>
 </div>`
+
+    let respostaEstaVisivel = false;
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel;
+        cartao.classList.toggle('active, respostaEstaVisivel')
+    }
+    cartao.addEventListener('click', viraCartao);
 
     container.appendChild(cartao);
 }
